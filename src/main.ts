260e59f1d2ac3cd3463a1e756f400acfb894038e
Dobroslav <dobroslav.radosavljevic@gmail.com>
2021-07-25 00:24:17 +0200

    First version of Twitter Tailwind Friend bot

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,65 @@
-/**
- * Some predefined delay values (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
-}
+import * as dotenv from "dotenv";
 
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
+dotenv.config()
+
+import TwitterApi, { ETwitterStreamEvent } from 'twitter-api-v2';
+
+const client = new TwitterApi(
+  {
+    appKey: process.env.TWITTER_ACCESS_TOKEN,
+    appSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    accessToken: process.env.TWITTER_CONSUMER_KEY,
+    accessSecret: process.env.TWITTER_CONSUMER_SECRET,
+  }
+);
+
+(async () => {
+
+  const me = await client.currentUser()
+
+  const trackList = ['@tailwindcss', '#tailwind', '#tailwindcss', '#tailwindui', '@tailwindui', '#headlessui']
+
+  const stream = await client.v1.filterStream({
+    track: trackList,
+  });
+
+  stream.autoReconnect = true
+
+  stream.autoReconnectRetries = 10
+
+  console.log('Starting stream');
+
+  // Awaits for a tweet
+  stream.on(
+    // Emitted when Node.js {response} emits a 'error' event (contains its payload).
+    ETwitterStreamEvent.ConnectionError,
+    err => console.log('Connection error!', err),
   );
-}
 
-// Below are examples of using ESLint errors suppression
-// Here it is suppressing a missing return type definition for the greeter function.
+  stream.on(
+    // Emitted when Node.js {response} is closed by remote or using .close().
+    ETwitterStreamEvent.ConnectionClosed,
+    () => console.log('Connection has been closed.'),
+  );
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export async function greeter(name: string) {
-  return await delayedHello(name, Delays.Long);
-}
+  stream.on(
+    // Emitted when a Twitter payload (a tweet or not, given the endpoint).
+    ETwitterStreamEvent.Data,
+    async eventData => {
+      console.log('Twitter has sent something:', eventData)
+
+      const { user, id_str } = eventData;
+
+      if (me.id_str !== user.id_str) {
+        await client.v2.follow(me.id_str, user.id_str).catch(err => console.log(err))
+        await client.v2.post(`users/${me.id_str}/retweets`, { tweet_id: id_str }).catch(err => console.log(err))
+      }
+    },
+  );
+
+  stream.on(
+    // Emitted when a Twitter sent a signal to maintain connection active
+    ETwitterStreamEvent.DataKeepAlive,
+    () => console.log('Twitter has a keep-alive packet.'),
+  );
+})();
